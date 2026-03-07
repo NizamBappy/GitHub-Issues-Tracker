9af9affeb3ce301e7164d410e7054c796ef6b9c4
NizamBappy <nizamcse66@gmail.com>
2026-03-07 20:57:16 +0300

    implement active botton

--- script/script.js
+++ script/script.js
@@ -1,29 +1,73 @@
 const cardContainer = document.getElementById("card-container");
-const totalIssues = document.getElementById("total-issues")
+const totalIssues = document.getElementById("total-issues");
+const btnContainer = document.getElementById("btn-container")
+const allBtn = document.getElementById("all-btn");
+const openBtn = document.getElementById("open-btn");
+const closeBtn = document.getElementById("close-btn");
+const allIssues =[];
 
 async function loadIssues(){
     const url ="https://phi-lab-server.vercel.app/api/v1/lab/issues"
 
     const res = await fetch(url)
     const data = await res.json();
-    displayIssues(data.data);
+    allIssues.push(...data.data);
+    displayIssues(allIssues);
 
 }
 
+function activeBtn(clickedBtn) {
+    
+    const buttons = document.querySelectorAll("#btn-container button");
+
+    buttons.forEach(btn => {
+        btn.classList.remove("btn-primary");
+        btn.classList.add("btn-outline");
+    });
+
+    clickedBtn.classList.add("btn-primary");
+    clickedBtn.classList.remove("btn-outline");
+}
+allBtn.addEventListener("click", ()=>{
+    activeBtn(allBtn);
+    cardContainer.innerHTML = "";
+    displayIssues(allIssues);
+});
+
+openBtn.addEventListener("click", ()=>{
+    activeBtn(openBtn);
+    const openIssues = allIssues.filter(issue => issue.status === "open");
+    cardContainer.innerHTML = "";
+    displayIssues(openIssues);
+
+});
+
+closeBtn.addEventListener("click", ()=>{
+    activeBtn(closeBtn);
+    const closedIssues = allIssues.filter(issue => issue.status === "closed");
+    cardContainer.innerHTML = "";
+    displayIssues(closedIssues);
+});
+
+
+
 function displayIssues(issues){
     // console.log(issues)
+    cardContainer.innerHTML = "";
+
     issues.forEach(issue =>{
         // console.log(issue)
+        
         const div = document.createElement("div")
-        div.innerHTML =`<div class="w-80 h-full bg-white rounded-xl shadow-md border-t-4 border-green-500 p-4 ">
+        div.innerHTML =`<div class="w-80 h-full bg-white rounded-xl shadow-md border-t-4  ${issue.status === "open" ? "border-green-500" : "border-red-500"} p-4 ">
 
                     <div class="flex justify-between items-center mb-3">
-                    <div class="w-8 h-8 bg-green-100 text-green-600 flex items-center justify-center rounded-full">
-                        <img src="./assets/Open-Status.png" alt="">
+                    <div>
+                       <img src="${issue.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed-Status.png"}" alt=""/>
                     </div>
 
                     <span class="bg-red-100 text-red-600 text-xs px-3 py-1 rounded-full font-semibold">
-                        HIGH
+                        ${issue.priority}
                     </span>
                     </div>
 
@@ -40,6 +84,7 @@ function displayIssues(issues){
                     </span>
 
                     <span class="text-xs bg-yellow-100 text-yellow-600 px-3 py-1 rounded-full font-medium">
+                        ${issue.labels}
                     </span>
                     </div>
 
@@ -57,6 +102,7 @@ function displayIssues(issues){
 function total (issues){
     // console.log(issues.length);
     // totalIssues.innerHTML =issues.length
+    totalIssues.innerHTML = "";
     const div = document.createElement("div");
     div.innerHTML = `<div class="flex items-center gap-5">
                 <div class="bg-[#ECE4FF] rounded-full p-3 flex items-center justify-center">
@@ -69,5 +115,7 @@ function total (issues){
             </div>`
     totalIssues.appendChild(div)
 }
+
+
 
 loadIssues();
