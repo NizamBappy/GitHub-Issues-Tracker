3b9f0d432068b29943a9b4d5676cb724b5ea1e09
NizamBappy <nizamcse66@gmail.com>
2026-03-07 22:15:20 +0300

    implement the issue details modal

--- script/script.js
+++ script/script.js
@@ -4,14 +4,18 @@ const btnContainer = document.getElementById("btn-container")
 const allBtn = document.getElementById("all-btn");
 const openBtn = document.getElementById("open-btn");
 const closeBtn = document.getElementById("close-btn");
+const spinner = document.getElementById("loading-spinner")
+const detailsModal = document.getElementById("details-container")
 const allIssues =[];
 
 async function loadIssues(){
+    showLoadingSpinner();
     const url ="https://phi-lab-server.vercel.app/api/v1/lab/issues"
 
     const res = await fetch(url)
     const data = await res.json();
     allIssues.push(...data.data);
+    hideLoadingSpinner();
     displayIssues(allIssues);
 
 }
@@ -30,26 +34,88 @@ function activeBtn(clickedBtn) {
 }
 allBtn.addEventListener("click", ()=>{
     activeBtn(allBtn);
+    showLoadingSpinner();
     cardContainer.innerHTML = "";
     displayIssues(allIssues);
+    hideLoadingSpinner();
 });
+
+
 
 openBtn.addEventListener("click", ()=>{
     activeBtn(openBtn);
+    showLoadingSpinner();
     const openIssues = allIssues.filter(issue => issue.status === "open");
     cardContainer.innerHTML = "";
+    hideLoadingSpinner();
     displayIssues(openIssues);
 
 });
 
 closeBtn.addEventListener("click", ()=>{
     activeBtn(closeBtn);
+    showLoadingSpinner();
     const closedIssues = allIssues.filter(issue => issue.status === "closed");
     cardContainer.innerHTML = "";
+    hideLoadingSpinner();
     displayIssues(closedIssues);
 });
 
+function showLoadingSpinner(){
+    spinner.classList.remove("hidden");
+    cardContainer.innerHTML = "";
+}
+function hideLoadingSpinner(){
+    spinner.classList.add("hidden");
+}
 
+async function loadIssueDetails(id){
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    // console.log(url);
+    const res = await fetch(url)
+    const data = await res.json()
+    displayIssueDetail(data.data);
+}
+
+function displayIssueDetail(issue){
+    console.log(issue)
+    detailsModal.innerHTML = `
+    <div class=" space-y-5">
+            <h1 class="text-3xl font-bold">${issue.title}</h1>
+            <div class="flex gap-3">
+                <button class="bg-green-600 text-white rounded full px-4 py-1s">${issue.status}</button>
+                <p class="font-semibold text-gray-400">. ${issue.assignee} .</p>
+                <p class="font-semibold text-gray-400">${issue.createdAt}</p>
+            </div>
+
+            <div>
+                <span class="text-xs bg-red-100 text-red-600 px-4 py-1 rounded-full font-medium">
+                            BUG
+                </span>
+
+                <span class="text-xs bg-yellow-100 text-yellow-600 px-4 py-1 rounded-full font-medium">
+                    Help Wanted
+                </span>
+            </div>
+
+            <p class="text-gray-400">${issue.description}</p>
+
+            <div class="flex gap-10 bg-[#F8FAFC] p-4">
+                <div>
+                    <p class="text-gray-400">Assigne:</p>
+                    <h3 class="font-bold text-2xl">${issue.assignee}</h3>
+                </div>
+                <div>
+                    <p>Priority:</p>
+                    <button class="bg-red-500 rounded-2xl text-white text-bold px-4 py-1">
+                    ${issue.priority}
+                    </button>
+                </div>
+            </div>
+
+        </div>`
+        document.getElementById("issue_modal").showModal();
+}
 
 function displayIssues(issues){
     // console.log(issues)
@@ -59,7 +125,7 @@ function displayIssues(issues){
         // console.log(issue)
         
         const div = document.createElement("div")
-        div.innerHTML =`<div class="w-80 h-full bg-white rounded-xl shadow-md border-t-4  ${issue.status === "open" ? "border-green-500" : "border-red-500"} p-4 ">
+        div.innerHTML =`<div class="w-80 h-full bg-white rounded-xl shadow-md border-t-4  ${issue.status === "open" ? "border-green-500" : "border-red-500"} p-4 " onclick="loadIssueDetails(${issue.id})">
 
                     <div class="flex justify-between items-center mb-3">
                     <div>
